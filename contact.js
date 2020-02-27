3f04400d6fd2f79c51f04ff65d9b6bfe4c934366
sebqlt <sebastien.quillet@outlook.com>
2020-02-27 15:33:41 -0800

    some progress

---
+++ contact.js
@@ -0,0 +1,16 @@
+export default[
+{
+contact:[
+    {
+    name:"John",
+    email:"johndoe@gmail.com",
+    phone:5625625621,
+},
+{
+    name:"Johne",
+    email:"johndoe@gmail.come",
+    phone:5625625622,
+}
+]
+},
+];
